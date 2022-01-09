6657f9e16f830e97a1bc7ae01d087846b3d02a8e
Karan Bisht <82514836+Karan554@users.noreply.github.com>
2022-01-09 11:13:44 +0530

    Update deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,6 +4,6 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i /opt/rsa /tmp/.auth centos@192.168.1.13:/tmp/.auth
-scp -i /opt/rsa ./jenkins/deploy/publish centos@192.168.1.13:/tmp/publish
-ssh -i /opt/rsa centos@192.168.1.13 "/tmp/publish"
+scp -i /opt/rsa /tmp/.auth centos@192.168.1.16:/tmp/.auth
+scp -i /opt/rsa ./jenkins/deploy/publish centos@192.168.1.16:/tmp/publish
+ssh -i /opt/rsa centos@192.168.1.16 "/tmp/publish"
