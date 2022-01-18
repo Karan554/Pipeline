16751f99ca9ab0a04ca3f882ea689dad989e1a85
Karan Bisht <82514836+Karan554@users.noreply.github.com>
2022-01-18 17:46:04 +0530

    Update deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -6,6 +6,6 @@ echo $PASS >> /tmp/.auth
 
 chmod 600 rsa
 
-scp -i /opt/rsa /tmp/.auth centos@1192.168.29.34:/tmp/.auth
+scp -i /opt/rsa /tmp/.auth centos@192.168.29.34:/tmp/.auth
 scp -i /opt/rsa ./jenkins/deploy/publish centos@192.168.29.34:/tmp/publish
-ssh -i /opt/rsa centos@1192.168.29.34 "/tmp/publish"
+ssh -i /opt/rsa centos@192.168.29.34 "/tmp/publish"
